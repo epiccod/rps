a100ad5c571e06f2c2332aeac5d74bf2689e801f
epiccod <147839812+epiccod@users.noreply.github.com>
2023-10-13 10:06:27 -0500

    Update script.js

--- rps/script.js
+++ rps/script.js
@@ -8,9 +8,25 @@ function computerPlay() {
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
+function disableButtons() {
+    buttons.forEach(elem => {
+        elem.disabled = true
+    })
+}
+
 function click(choice) {
     if (!choices.includes(choice)) throw new Error("Unknown choice chosen");
     const computerChoice = computerPlay();
     if (choice === computerChoice) console.log("tie");
     else (choice === "rock")
+}
+
+if (computerscore == 5){
+    result += '<br><br>The computer beat you dumbass. Reload the page to play again'
+    disablebuttons()
+}
+
+if (playerscore == 5){
+    result += '<br><br>You won the game! Reload the page to play again'
+    disablebuttons()
 }
