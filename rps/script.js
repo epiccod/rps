c96faa8648123984b86b276d6cabbbf9c12ad75b
epiccod <147839812+epiccod@users.noreply.github.com>
2023-11-03 09:45:02 -0500

    Update script.js

--- rps/script.js
+++ rps/script.js
@@ -1,11 +1,10 @@
-let playerscore = 0;
-let computerscore = 0;
-
-const buttons = document.querySelectorAll('input');
-const choices = ['rock', 'paper', 'scissors'];
+let playerScore = 0
+let computerScore = 0
+const buttons = document.querySelectorAll('input')
 
 function computerPlay() {
-    return choices[Math.floor(Math.random() * choices.length)];
+    let choices = ['rock', 'paper', 'scissors']
+    return choices[Math.floor(Math.random() * choices.length)]
 }
 
 function disableButtons() {
@@ -14,19 +13,44 @@ function disableButtons() {
     })
 }
 
-function click(choice) {
-    if (!choices.includes(choice)) throw new Error("Unknown choice chosen");
-    const computerChoice = computerPlay();
-    if (choice === computerChoice) console.log("tie");
-    else (choice === "rock")
+function playRound(playerSelection) {
+    let computerSelection = computerPlay()
+    let result = ""
+
+    if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
+        (playerSelection == 'scissors' && computerSelection == 'paper') ||
+        (playerSelection == 'paper' && computerSelection == 'rock')) {
+        
+        playerScore += 1
+        result = ('You win! ' + playerSelection + ' beats ' + computerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
+
+        if (playerScore == 5) {
+            result += '<br><br>You beat a robot no one gives a shit.'
+            disableButtons()
+        }
+    }
+    else if (playerSelection == computerSelection) {
+        result = ('It\'s a tie. You both chose ' + playerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
+    }
+    else {
+        computerScore += 1
+        result = ('You lose! ' + computerSelection + ' beats ' + playerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
+
+        if (computerScore == 5) {
+            result += '<br><br>you lost to a robot dumbass. I honestly can not believe youre this stupid. shameful'
+            disableButtons()
+        }
+    }
+
+    document.getElementById('result').innerHTML = result
+    return
 }
 
-if (computerscore == 5){
-    result += '<br><br>The computer beat you dumbass. Reload the page to play again'
-    disablebuttons()
-}
-
-if (playerscore == 5){
-    result += '<br><br>You won the game! Reload the page to play again'
-    disablebuttons()
-}
+buttons.forEach(button =>{
+    button.addEventListener('click', function(){
+        playRound(button.value)
+    })
+})
